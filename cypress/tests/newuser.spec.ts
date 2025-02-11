c685790949c988b036027a92d87280804dad0558
Érica Fernandes Anfilóquio <ericafanfiloquio@gmail.com>
2025-02-11 23:20:44 +0100

    New User tests

--- cypress/tests/newuser.spec.ts
+++ cypress/tests/newuser.spec.ts
@@ -1,19 +1,60 @@
 describe("Real World App Tests", () => {
   it("Creating New User", () => {
     cy.visit("http://localhost:3000/signin");
-    cy.get('[data-test="signup"]').click();
+    cy.get("[href='/signup']").click();
     cy.get(".SignUpForm-paper");
-    cy.get("#firstName").type("Joana");
-    cy.get("#lastName").type("Santos");
-    cy.get("#username").type("joanasantos");
-    cy.get("#password").type("testando");
-    cy.get("#confirmPassword").type("testando");
-    cy.get('[data-test="signup-submit"]').click();
+    cy.get("[name='firstName']").type("Joana");
+    cy.get("[name='lastName']").type("Santos");
+    cy.get("[name='username']").type("joanasantos");
+    cy.get("[name='password']").type("testando");
+    cy.get("[name='confirmPassword']").type("testando");
+    cy.get("[type='submit']").click();
     cy.get(".SignInForm-paper");
     cy.visit("http://localhost:3000/signin");
     cy.get("[name='username']").type("joanasantos");
     cy.get("[name='password']").type("testando");
-    cy.get('[data-test="signin-submit"]').click();
+    cy.get("[type='submit']").click();
     cy.get(".css-1idn90j-MuiGrid-root");
+  
   });
+
+  it("Creating New User Unsuccessfuly", () => {
+    cy.visit("http://localhost:3000/signin");
+    cy.get("[href='/signup']").click();
+    cy.get('.SignUpForm-form');
+    cy.get("[name='firstName']").type("Mark");
+    cy.get("[name='lastName']").type("Rock");
+    cy.get("[name='username']").click();
+    cy.get("[name='password']").type("1542");
+    cy.get("[name='confirmPassword']").type("1542");
+    cy.get(".Mui-error")
+    cy.get("[type='submit']")
+  });
+
+  it("Creating New User Unsuccessfuly", () => {
+    cy.visit("http://localhost:3000/signin");
+    cy.get("[href='/signup']").click();
+    cy.get('.SignUpForm-form');
+    cy.get("[name='firstName']").type("Mark");
+    cy.get("[name='lastName']").click();
+    cy.get("[name='username']").type("markrock");
+    cy.get("[name='password']").type("1542");
+    cy.get("[name='confirmPassword']").type("1542");
+    cy.get(".Mui-error");
+    cy.get("[type='submit']");
+  });
+
+  it("Creating New User Unsuccessfuly", () => {
+    cy.visit("http://localhost:3000/signin");
+    cy.get("[href='/signup']").click();
+    cy.get('.SignUpForm-form');
+    cy.get("[name='firstName']").type("Mark");
+    cy.get("[name='lastName']").type("Rock");
+    cy.get("[name='username']").type("markrock");
+    cy.get("[name='password']").type("154");
+    cy.get("[name='confirmPassword']").type("154");
+    cy.get(".Mui-error");
+    cy.get("[type='submit']").click();
+  });
+
 });
