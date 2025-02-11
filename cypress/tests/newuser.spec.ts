923fce39ae6a590767b6ef5b04765467e37e257d
Érica Fernandes Anfilóquio <ericafanfiloquio@gmail.com>
2025-02-11 20:20:00 +0100

    Format of files corrected

--- cypress/tests/newuser.spec.ts
+++ cypress/tests/newuser.spec.ts
@@ -1,19 +1,19 @@
-describe('Real World App Tests', () => {
-    it('Creating New User', () => {
-      cy.visit('http://localhost:3000/signin')
-      cy.get('[data-test="signup"]').click()
-      cy.get('.SignUpForm-paper')
-      cy.get('#firstName').type('Joana')
-      cy.get('#lastName').type('Santos')
-      cy.get('#username').type('joanasantos')
-      cy.get('#password').type('testando')
-      cy.get('#confirmPassword').type('testando')
-      cy.get('[data-test="signup-submit"]').click()
-      cy.get('.SignInForm-paper')
-      cy.visit('http://localhost:3000/signin')
-      cy.get("[name='username']").type('joanasantos')
-      cy.get("[name='password']").type('testando')
-      cy.get('[data-test="signin-submit"]').click()
-      cy.get('.css-1idn90j-MuiGrid-root')
-    })
-})
+describe("Real World App Tests", () => {
+  it("Creating New User", () => {
+    cy.visit("http://localhost:3000/signin");
+    cy.get('[data-test="signup"]').click();
+    cy.get(".SignUpForm-paper");
+    cy.get("#firstName").type("Joana");
+    cy.get("#lastName").type("Santos");
+    cy.get("#username").type("joanasantos");
+    cy.get("#password").type("testando");
+    cy.get("#confirmPassword").type("testando");
+    cy.get('[data-test="signup-submit"]').click();
+    cy.get(".SignInForm-paper");
+    cy.visit("http://localhost:3000/signin");
+    cy.get("[name='username']").type("joanasantos");
+    cy.get("[name='password']").type("testando");
+    cy.get('[data-test="signin-submit"]').click();
+    cy.get(".css-1idn90j-MuiGrid-root");
+  });
+});
