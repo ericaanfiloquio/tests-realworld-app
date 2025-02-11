dc7c7919bc1c2fc629d118e698a28b4c6ff6463b
Érica Fernandes Anfilóquio <ericafanfiloquio@gmail.com>
2025-02-11 07:29:21 +0100

    First Login Tests RWA

---
+++ cypress/tests/login.spec.ts
@@ -0,0 +1,17 @@
+describe('Real World App Tests', () => {
+  it('Login Success', () => {
+    cy.visit('http://localhost:3000/signin')
+    cy.get("[name='username']").type('Heath93')
+    cy.get("[name='password']").type('s3cret')
+    cy.get('[data-test="signin-submit"]').click()
+    cy.get('.css-1idn90j-MuiGrid-root')
+  })
+
+  it('Login Fail', () => {
+    cy.visit('http://localhost:3000/signin')
+    cy.get("[name='username']").type('Romeu')
+    cy.get("[name='password']").type('s3cret')
+    cy.get('[data-test="signin-submit"]').click()
+    cy.get('[data-test="signin-error"]')
+  })
+})
